41bd2535ab902862228576b398ba42c7a6362910
addis g <addis.a.gn@gmail.com>
2021-06-24 18:15:01 -0400

    returned the 5002 port to 5000

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ server.use("/api", require("./routes"));
 const { client } = require("./db");
 
 // connect to server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, async () => {
   console.log(`Server is running on ${PORT}!`);
 
