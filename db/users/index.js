96b2ff00654aa53073d468da3a59d7bb767ae445
addis g <addis.a.gn@gmail.com>
2021-06-26 12:17:49 -0400

    changed createUser in db/users/index.js

--- db/users/index.js
+++ db/users/index.js
@@ -1,8 +1,10 @@
 const getUserByUserName = require("./getUserByUserName");
+const createUser = require("./createUser.js");
 
 module.exports = {
-  ...require("./createUser.js"),
+  //...require("./createUser.js"),
   ...require("./getUser.js"),
   //...require("./getUserByUserName.js"),
   getUserByUserName,
+  createUser,
 };
