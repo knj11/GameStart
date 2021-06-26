01621b68a11a1f4c3f1e15b1219d9d14a82bb679
Koby Johnson <knj11@vt.edu>
2021-06-26 09:55:37 -0400

    createNewUser front end request

---
+++ src/auth/createNewUser.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const createUserRoute = "/api/users"
+
+export async function createNewUser({username, password}) {
+  try {
+    const user = await axios.post(createUserRoute, {username, password})
+    localStorage.setItem("user", JSON.stringify(user))
+    return user
+  } catch (error) {
+    console.log("trouble creating new user")
+    console.dir(error)
+  }
+}
