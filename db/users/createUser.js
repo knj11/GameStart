73ffab584409e00e2981ae20f65399c4b48a5947
Jason <60307987+jlcolli79@users.noreply.github.com>
2021-06-26 10:26:25 -0400

    added user functions

---
+++ db/users/createUser.js
@@ -0,0 +1,29 @@
+const client = require("../index");
+const bcrypt = require("bcrypt");
+
+async function createUser({ firstName, lastName, description, email, password, roleId }) {
+  SALT_COUNT = 10;
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      INSERT INTO users("firstName", "lastName", description, email, "hashedPassword", "roleId" )
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING *
+    `[(firstName, lastName, description, email, hashedPassword, roleId)]
+    );
+
+    if (user.password) {
+      delete user.password;
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = createUser;
---
+++ db/users/getUserByUserName.js
@@ -0,0 +1,26 @@
+const client = require("../index");
+
+async function getUserByUserName(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT * 
+      FROM users
+      WHERE username = $1
+    `[username]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createUser,
+  getUserByUserName,
+};
+
+module.exports = getUserByUserName;
