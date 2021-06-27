59e593811e4bc8edb4accc31cdf779f413db7ec8
Koby Johnson <knj11@vt.edu>
2021-06-27 15:12:38 -0400

    fixing client bug in getAllProducts

--- db/products/getAllProducts.js
+++ db/products/getAllProducts.js
@@ -1,11 +1,12 @@
-const { client } = require("../index");
+//For some reason when you require client from index.js file it does not work...
+const { client } = require("../client");
 
 async function getAllProducts() {
   try {
     const { rows: products } = await client.query(
       /*sql*/
       ` SELECT *
-        FROM products 
+        FROM products;
       `
     );
     return products;
