f47e7c32e07a464357a746017107ce6675e95c57
Koby Johnson <knj11@vt.edu>
2021-06-28 09:20:51 -0400

    Adding an Express error Handler

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const server = express();
 
+require('dotenv').config()
+
 // logging
 
 const morgan = require("morgan");
@@ -14,6 +16,14 @@ server.use(express.json());
 
 // API
 server.use("/api", require("./routes"));
+
+//Error Handling
+server.use((error, _req, res, _next) => {
+  console.log("Sending Error Message")
+  console.log("Error", error)
+  res.status(error.status)
+  res.send(error)
+});
 
 // DB connection
 const { client } = require("./db");
