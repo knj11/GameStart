7d0dbc5d676ed5530b0ca67462283a777e4461d7
Koby Johnson <knj11@vt.edu>
2021-07-11 08:50:56 -0400

    fixing Admin delete functionality

--- db/products/deleteProduct.js
+++ db/products/deleteProduct.js
@@ -1,10 +1,23 @@
 const { client } = require("../client");
 
 async function deleteProduct(id) {
-    try {
-        const {
-            rows: [deletedProduct],
-          } =
+  try {
+    //Remove product dependencies
+    await client.query(`DELETE FROM "ordersItem" WHERE "productId"=$1;`, [id])
+    await client.query(`DELETE FROM "reviews" WHERE "productId"=$1;`, [id])
+    await client.query(`DELETE FROM "productCategory" WHERE "productId"=$1;`, [id])
+
+    //Remove inventory dependencies
+    const {rows: inventoryIds} = await client.query(`SELECT id FROM "inventory" WHERE "productId"=$1;`, [id])
+    for (const i of inventoryIds) {
+      await client.query(`DELETE FROM "ordersItem" WHERE "inventoryId"=$1;`, [i.id])
+    } 
+    await client.query(`DELETE FROM "inventory" WHERE "productId"=$1;`, [id])
+
+    //Remove from Main product table
+    const {
+      rows: [deletedProduct],
+    } =
       await client.query(
         /*sql*/ `
           DELETE FROM products WHERE id=$1
@@ -12,12 +25,12 @@ async function deleteProduct(id) {
           `,
         [id]
       );
-      return deletedProduct;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    return deletedProduct;
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
+}
 
 module.exports = { deleteProduct };
 
